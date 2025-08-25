add2b3c0e404fa8a00ee1b782007e97458b6284e
Alejandro Diez Bermejo <alejandrodiezbermejo@gmail.com>
2025-08-26 00:15:20 +0200

    Add auth schemas and refactor triggers

--- src/db/migrations/0002_migration.sql
+++ src/db/migrations/0002_migration.sql
@@ -20,64 +20,55 @@ CREATE TRIGGER `validate_comment_stage`
 BEFORE INSERT ON `comments`
 FOR EACH ROW
 WHEN NEW.`stage_id` IS NOT NULL
+    AND NOT EXISTS (
+        SELECT 1
+        FROM `stages`
+        WHERE `id` = NEW.`stage_id` AND `trip_id` = NEW.`trip_id`
+    )
 BEGIN
-    SELECT
-        CASE
-            WHEN NOT EXISTS (
-                SELECT 1 FROM `stages`
-                WHERE `id` = NEW.`stage_id` AND `trip_id` = NEW.`trip_id`
-            )
-        THEN RAISE(ABORT, 'FOREIGN KEY constraint failed')
-    END;
+    SELECT RAISE(ABORT, 'FOREIGN KEY constraint failed');
 END;
 DROP TRIGGER IF EXISTS `validate_comment_stage_on_update`;
 CREATE TRIGGER `validate_comment_stage_on_update`
 BEFORE UPDATE ON `comments`
 FOR EACH ROW
 WHEN NEW.`stage_id` IS NOT NULL
+    AND NOT EXISTS (
+        SELECT 1
+        FROM `stages`
+        WHERE `id` = NEW.`stage_id` AND `trip_id` = NEW.`trip_id`
+    )
 BEGIN
-    SELECT
-        CASE
-            WHEN NOT EXISTS (
-                SELECT 1 FROM `stages`
-                WHERE `id` = NEW.`stage_id` AND `trip_id` = NEW.`trip_id`
-            )
-        THEN RAISE(ABORT, 'FOREIGN KEY constraint failed')
-    END;
+    SELECT RAISE(ABORT, 'FOREIGN KEY constraint failed');
 END;
 DROP TRIGGER IF EXISTS `validate_trip_allow_comments`;
 CREATE TRIGGER `validate_trip_allow_comments`
 BEFORE INSERT ON `comments`
 FOR EACH ROW
 WHEN NEW.`stage_id` IS NULL
+    AND NOT EXISTS (
+        SELECT 1
+        FROM `trips`
+        WHERE `id` = NEW.`trip_id`
+            AND `allow_comments` = 1
+    )
 BEGIN
-    SELECT
-        CASE
-            WHEN NOT EXISTS (
-                SELECT 1
-                FROM `trips`
-                WHERE `id` = NEW.`trip_id`
-                  AND `allow_comments` = 1
-            )
-        THEN RAISE(ABORT, 'Trip does not allow comments')
-    END;
+    SELECT RAISE(ABORT, 'Trip does not allow comments');
 END;
 DROP TRIGGER IF EXISTS `validate_stage_allow_comments`;
 CREATE TRIGGER `validate_stage_allow_comments`
 BEFORE INSERT ON `comments`
 FOR EACH ROW
 WHEN NEW.`stage_id` IS NOT NULL
+    AND NOT EXISTS (
+        SELECT 1
+        FROM `stages`
+        WHERE `trip_id` = NEW.`trip_id`
+            AND `id` = NEW.`stage_id`
+            AND `allow_comments` = 1
+    )
 BEGIN
-    SELECT
-        CASE
-            WHEN NOT EXISTS (
-                SELECT 1
-                FROM `stages`
-                WHERE `trip_id` = NEW.`trip_id` AND `id` = NEW.`stage_id`
-                  AND `allow_comments` = 1
-            )
-        THEN RAISE(ABORT, 'Stage does not allow comments')
-    END;
+    SELECT RAISE(ABORT, 'Stage does not allow comments');
 END;
 DROP TRIGGER IF EXISTS `delete_comments_on_stage_deleted`;
 CREATE TRIGGER `delete_comments_on_stage_deleted`
@@ -86,7 +77,7 @@ FOR EACH ROW
 BEGIN
     DELETE FROM `comments`
     WHERE `stage_id` = OLD.`id`
-      AND `trip_id` = OLD.`trip_id`;
+        AND `trip_id` = OLD.`trip_id`;
 END;
 DROP TRIGGER IF EXISTS `update_modified_at_on_comments`;
 CREATE TRIGGER `update_modified_at_on_comments`
